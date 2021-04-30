5bf812d1e93657c4415a6c1239a6e6e7e905d748
Gabriel Izaias <gabriel.izaias@gmail.com>
2021-04-30 10:36:22 +0200

    log: update default fns to pass `args` correctly

--- log/mod.ts
+++ log/mod.ts
@@ -12,48 +12,48 @@ export * from "./levels.ts";
  * Log to stdout with trace level
  */
 export async function trace(...msg: unknown[]): Promise<void> {
-  await write(Deno.stdout, gray(Level.TRACE), msg);
+  await write(Deno.stdout, gray(Level.TRACE), ...msg);
 }
 
 /**
  * Log to stdout with debug level
  */
 export async function debug(...msg: unknown[]): Promise<void> {
-  await write(Deno.stdout, gray(Level.DEBUG), msg);
+  await write(Deno.stdout, gray(Level.DEBUG), ...msg);
 }
 
 /**
  * Log to stdout with info level
  */
 export async function info(...msg: unknown[]): Promise<void> {
-  await write(Deno.stdout, blue(Level.INFO), msg);
+  await write(Deno.stdout, blue(Level.INFO), ...msg);
 }
 
 /**
  * Log to stdout with warn level
  */
 export async function warn(...msg: unknown[]): Promise<void> {
-  await write(Deno.stdout, yellow(Level.WARN), msg);
+  await write(Deno.stdout, yellow(Level.WARN), ...msg);
 }
 
 /**
  * Log to stderr with error level
  */
 export async function error(...msg: unknown[]): Promise<void> {
-  await write(Deno.stderr, red(Level.ERROR.toUpperCase()), msg);
+  await write(Deno.stderr, red(Level.ERROR.toUpperCase()), ...msg);
 }
 
 /**
  * Log to stderr with fatal level
 */
 export async function fatal(...msg: unknown[]): Promise<void> {
-  await write(Deno.stderr, red(Level.FATAL.toUpperCase()), msg);
+  await write(Deno.stderr, red(Level.FATAL.toUpperCase()), ...msg);
 }
 
 async function write(
   output: Deno.Writer,
-  level: Level | string,
-  msg: unknown[],
+  prefix: string,
+  ...msg: unknown[]
 ): Promise<void> {
-  await output.write(new TextEncoder().encode(`${level} ${msg}\n`));
+  await output.write(new TextEncoder().encode(`${prefix} ${msg}\n`));
 }
