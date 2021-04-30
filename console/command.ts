932b15e4381ac1b896cd05d808b88c1a46d79f55
Gabriel Izaias <gabriel.izaias@gmail.com>
2021-04-30 10:35:13 +0200

    console: make command handler params of type `unknown`

--- console/command.ts
+++ console/command.ts
@@ -1,5 +1,3 @@
-import type { Args } from "https://deno.land/std@0.95.0/flags/mod.ts";
-
 export type Command = {
   /** The console command name. */
   name: string;
@@ -10,8 +8,5 @@ export type Command = {
   /** Indicates whether the command should be shown in the commands list. */
   hidden?: boolean;
   /** The command handler function. */
-  handler: (
-    /** Parsed Deno.args object */
-    args: Args,
-  ) => Promise<void>;
+  handler: (args: unknown) => Promise<void>;
 };
