da3aa1c6b03c3a93e475255ca20f983d7c543d16
Gabriel Silva <gabriel.izaias@gmail.com>
2022-04-01 21:29:01 +0200

    fix(log): update import path for asserts

--- log/message.test.ts
+++ log/message.test.ts
@@ -1,8 +1,6 @@
-import { asserts } from "../deps.ts";
+import { assertEquals } from "testing/asserts.ts";
 import { LogLevel } from "./level.ts";
 import { LogMessage } from "./message.ts";
-
-const { assertEquals } = asserts;
 
 Deno.test("log message", () => {
   const message = new LogMessage(LogLevel.DEBUG, "Hello World!");
