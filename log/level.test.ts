116513e41e0bb8ad44a7c7cb739a7b0600a91cf9
Gabriel Silva <gabriel.izaias@gmail.com>
2022-04-01 22:54:29 +0200

    chore(log): cleanup test for log level

--- log/level.test.ts
+++ log/level.test.ts
@@ -1,42 +1,42 @@
 import { assertEquals } from "testing/asserts.ts";
 import { getLevelValue, LogLevel } from "./level.ts";
 
-Deno.test("log level: emergency", () => {
+Deno.test("log level emergency", () => {
   assertEquals(LogLevel.EMERGENCY, "emergency");
   assertEquals(getLevelValue(LogLevel.EMERGENCY), 0);
 });
 
-Deno.test("log level: alert", () => {
+Deno.test("log level alert", () => {
   assertEquals(LogLevel.ALERT, "alert");
   assertEquals(getLevelValue(LogLevel.ALERT), 1);
 });
 
-Deno.test("log level: critical", () => {
+Deno.test("log level critical", () => {
   assertEquals(LogLevel.CRITICAL, "critical");
   assertEquals(getLevelValue(LogLevel.CRITICAL), 2);
 });
 
-Deno.test("log level: error", () => {
+Deno.test("log level error", () => {
   assertEquals(LogLevel.ERROR, "error");
   assertEquals(getLevelValue(LogLevel.ERROR), 3);
 });
 
-Deno.test("log level: warn", () => {
+Deno.test("log level warn", () => {
   assertEquals(LogLevel.WARNING, "warning");
   assertEquals(getLevelValue(LogLevel.WARNING), 4);
 });
 
-Deno.test("log level: notice", () => {
+Deno.test("log level notice", () => {
   assertEquals(LogLevel.NOTICE, "notice");
   assertEquals(getLevelValue(LogLevel.NOTICE), 5);
 });
 
-Deno.test("log level: info", () => {
+Deno.test("log level info", () => {
   assertEquals(LogLevel.INFO, "info");
   assertEquals(getLevelValue(LogLevel.INFO), 6);
 });
 
-Deno.test("log level: debug", () => {
+Deno.test("log level debug", () => {
   assertEquals(LogLevel.DEBUG, "debug");
   assertEquals(getLevelValue(LogLevel.DEBUG), 7);
 });
