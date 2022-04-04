ce8a52a621736a539ba5053ede3777652c4788c1
Gabriel Silva <gabriel.izaias@gmail.com>
2022-04-05 00:31:22 +0200

    chore(log): improve docblock for logger and level

--- log/logger.ts
+++ log/logger.ts
@@ -16,39 +16,47 @@ export class Logger {
     this.handlers = options?.handlers ?? [];
   }
 
+  /** Logs a message with emergency level */
   emergency(message: LogMessage["value"]): void {
     this.#log(LogLevel.EMERGENCY, message);
   }
 
+  /** Logs a message with alert level */
   alert(message: LogMessage["value"]): void {
     this.#log(LogLevel.ALERT, message);
   }
 
+  /** Logs a message with critical level */
   critical(message: LogMessage["value"]): void {
     this.#log(LogLevel.CRITICAL, message);
   }
 
+  /** Logs a message with error level */
   error(message: LogMessage["value"]): void {
     this.#log(LogLevel.ERROR, message);
   }
 
+  /** Logs a message with warning level */
   warning(message: LogMessage["value"]): void {
     this.#log(LogLevel.WARNING, message);
   }
 
+  /** Logs a message with notice level */
   notice(message: LogMessage["value"]): void {
     this.#log(LogLevel.NOTICE, message);
   }
 
+  /** Logs a message with info level */
   info(message: LogMessage["value"]): void {
     this.#log(LogLevel.INFO, message);
   }
 
+  /** Logs a message with debug level */
   debug(message: LogMessage["value"]): void {
     this.#log(LogLevel.DEBUG, message);
   }
 
-  #log(level: LogMessage["level"], message: LogMessage["value"]): void {
+  #log(level: LogLevel, message: LogMessage["value"]): void {
     const messageObject = new LogMessage(level, message);
 
     for (const handler of this.handlers) {
