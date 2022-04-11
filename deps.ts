81b47b346aeaed55c96b060cdf734dc5a083c3fa
Gabriel Silva <gabriel.izaias@gmail.com>
2022-04-11 22:17:43 +0200

    deps: update std to 0.134.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as colors from "https://deno.land/std@0.133.0/fmt/colors.ts";
-export * as asserts from "https://deno.land/std@0.133.0/testing/asserts.ts";
-export * as flags from "https://deno.land/std@0.133.0/flags/mod.ts";
+export * as colors from "https://deno.land/std@0.134.0/fmt/colors.ts";
+export * as asserts from "https://deno.land/std@0.134.0/testing/asserts.ts";
+export * as flags from "https://deno.land/std@0.134.0/flags/mod.ts";
