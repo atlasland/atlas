e45df3f42db29522a7c41f6f7e670c14a37affd3
Gabriel Silva <gabriel.izaias@gmail.com>
2022-07-14 17:03:16 -0300

    chore(website): add barebones homepage

---
+++ website/deps.ts
@@ -0,0 +1,3 @@
+export { serve } from "https://deno.land/std@0.148.0/http/server.ts";
+export { extname } from "https://deno.land/std@0.148.0/path/mod.ts";
+export { contentType } from "https://deno.land/std@0.148.0/media_types/mod.ts";
