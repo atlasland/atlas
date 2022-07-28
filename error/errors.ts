263e7b4e377eae7c0cae84223bf3a056a5c8ac2d
Gabriel Silva <gabriel.izaias@gmail.com>
2022-07-28 12:50:12 -0300

    feat(error): add MissingArgumentError

--- error/errors.ts
+++ error/errors.ts
@@ -4,3 +4,10 @@ export abstract class BaseError extends Error {
     Object.setPrototypeOf(this, BaseError.prototype);
   }
 }
+
+export class MissingArgumentError extends BaseError {
+  constructor(argument?: string) {
+    super(`Missing argument${argument ? `: ${argument}` : ""}`);
+    Object.setPrototypeOf(this, MissingArgumentError.prototype);
+  }
+}
---
+++ error/errors_test.ts
@@ -0,0 +1,13 @@
+import { assertEquals, assertThrows } from "./deps.ts";
+import { MissingArgumentError } from "./errors.ts";
+
+Deno.test({
+  name: "[error] MissingArgumentError",
+  fn: () => {
+    const error = new MissingArgumentError("argument_name");
+    assertEquals(error.message, "Missing argument: argument_name");
+    assertThrows(() => {
+      throw error;
+    }, MissingArgumentError);
+  }
+});
