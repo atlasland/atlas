4cc22b08f428e8e698d8300592c6e5262166dced
Gabriel Silva <gabriel.izaias@gmail.com>
2022-08-12 16:04:06 -0300

    chore: update std dependencies to 0.152.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,2 @@
-export * as colors from "https://deno.land/std@0.144.0/fmt/colors.ts";
-export * as asserts from "https://deno.land/std@0.144.0/testing/asserts.ts";
-export * as flags from "https://deno.land/std@0.144.0/flags/mod.ts";
+export * as asserts from "https://deno.land/std@0.152.0/testing/asserts.ts";
+export * as colors from "https://deno.land/std@0.152.0/fmt/colors.ts";
