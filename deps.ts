c5321f54f35131ca323b79624488dcbbd6916e33
Gabriel Silva <gabriel.izaias@gmail.com>
2022-08-17 19:54:14 -0300

    fix(cli): missing flags parser

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 export * as asserts from "https://deno.land/std@0.152.0/testing/asserts.ts";
 export * as colors from "https://deno.land/std@0.152.0/fmt/colors.ts";
+export * as flags from "https://deno.land/std@0.152.0/flags/mod.ts";
