065f363fafcbfdf026b1a65a0c70374a3d17beba
Gabe <gabriel.izaias@gmail.com>
2022-08-17 20:16:43 -0300

    test(error): improve test descriptions (#19)

--- error/errors_test.ts
+++ error/errors_test.ts
@@ -1,14 +1,16 @@
 import { assertEquals, assertThrows } from "./deps.ts";
 import { BaseError } from "./errors.ts";
 
-Deno.test({
-	name: "[error] BaseError",
-	fn: () => {
-		class TestError extends BaseError {}
-		const error = new TestError("test error!");
-		assertEquals(error.message, "test error!");
-		assertThrows(() => {
-			throw error;
-		}, Error);
-	},
+Deno.test("BaseError accepts a message", () => {
+	class TestError extends BaseError {}
+	const error = new TestError("test error!");
+	assertEquals(error.message, "test error!");
+});
+
+Deno.test("BaseError inherits from Error", () => {
+	class TestError extends BaseError {}
+	const error = new TestError("test error!");
+	assertThrows(() => {
+		throw error;
+	}, Error);
 });
