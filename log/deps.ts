eaad7541191e610eae49aff728b5658294854e97
Gabriel Silva <gabriel.izaias@gmail.com>
2022-10-12 09:38:47 -0300

    chore: update std to 0.159.0

--- log/deps.ts
+++ log/deps.ts
@@ -1,2 +1,2 @@
-export * as colors from "https://deno.land/std@0.155.0/fmt/colors.ts";
-export * as flags from "https://deno.land/std@0.155.0/flags/mod.ts";
+export * as colors from "https://deno.land/std@0.159.0/fmt/colors.ts";
+export * as flags from "https://deno.land/std@0.159.0/flags/mod.ts";
