06b2bd9c85e6b94bd22ede6963d598e8ebc32e7a
Gabriel Silva <git@gabe.id>
2023-05-12 15:36:49 -0300

    build: update `std` to `0.187.0`

--- website/deps.ts
+++ website/deps.ts
@@ -1,2 +1,2 @@
-export { serve } from "https://deno.land/std@0.186.0/http/server.ts";
-export { serveFile } from "https://deno.land/std@0.186.0/http/file_server.ts";
+export { serve } from "https://deno.land/std@0.187.0/http/server.ts";
+export { serveFile } from "https://deno.land/std@0.187.0/http/file_server.ts";
