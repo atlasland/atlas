3b950e77e21fd61526bc5adc0e218e58ba1baa5e
Gabriel Silva <git@gabe.id>
2023-11-22 17:23:49 +0100

    build: update `std` to `0.207.0`

--- deps_dev.ts
+++ deps_dev.ts
@@ -1,5 +1,6 @@
 export {
+	assert,
 	assertEquals,
 	assertStringIncludes,
 	assertThrows,
-} from "https://deno.land/std@0.187.0/testing/asserts.ts";
+} from "https://deno.land/std@0.207.0/testing/asserts.ts";
--- http/deps.ts
+++ http/deps.ts
@@ -1,13 +1,7 @@
-export {
-	type ConnInfo,
-	serve,
-	type ServeInit,
-	Status,
-	STATUS_TEXT,
-} from "https://deno.land/std@0.197.0/http/mod.ts";
-export { serveFile } from "https://deno.land/std@0.197.0/http/file_server.ts";
-export { walk, type WalkOptions } from "https://deno.land/std@0.197.0/fs/mod.ts";
-export { extname, toFileUrl } from "https://deno.land/std@0.197.0/path/mod.ts";
+export { type ConnInfo, Status, STATUS_TEXT } from "https://deno.land/std@0.207.0/http/mod.ts";
+export { serveFile } from "https://deno.land/std@0.207.0/http/file_server.ts";
+export { walk, type WalkOptions } from "https://deno.land/std@0.207.0/fs/mod.ts";
+export { extname, toFileUrl } from "https://deno.land/std@0.207.0/path/mod.ts";
 
 import { ConsoleHandler, Logger } from "../log/mod.ts";
 export const logger = new Logger("atlas/http", {
